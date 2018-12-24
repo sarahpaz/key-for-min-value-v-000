2f10a2a3fc0bdaad3a7c51687effb5f371911fca
Sarah Paz <sarah.paz@gmail.com>
2018-12-24 17:38:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,11 @@
 
 def key_for_min_value(hash)
   lowest_key = nil
-  lowest_value = Float::INFINITY
+  lowest_value = nil
   hash.each do |key, value|
-    if value < lowest_value
+    if lowest_value == nil || value < lowest_value
       lowest_value = value
       lowest_key = key
-    else
-      nil
     end
   end
   lowest_key
